ca5240a7ffa30b4135b4247e00031188fcabaec4
Ega Erinovian <72180780+ega-erinovian@users.noreply.github.com>
2024-12-13 14:00:52 +0700

    fix: fixed event select input at create voucher form (#11)

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,8 @@
 import LoginComponent from "@/features/auth/login";
 
 const Login = () => {
-  return <LoginComponent />;
+  // return <LoginComponent />;
+  return <h1>Login</h1>;
 };
 
 export default Login;
